78fa967531b499eace5aa759f0d7e309060879cf
Garga180 <bengerge@gmail.com>
2026-09-01 23:45:04 +0200

    Fix scroll to top on navigation

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import localeHu from '@angular/common/locales/hu';
 import { routes } from './app.routes';
@@ -12,7 +12,7 @@ registerLocaleData(localeHu);
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })),
     provideHttpClient(withInterceptors([authInterceptor])),
     { provide: LOCALE_ID, useValue: 'hu-HU' }
   ]
