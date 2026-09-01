f208e2a6aeac52536f66fd3456e2b232bf48e8a7
Garga180 <bengerge@gmail.com>
2026-09-02 00:36:14 +0200

    Force IPv4 for Resend API to fix Network is unreachable

--- Backend/email_service.py
+++ Backend/email_service.py
@@ -1,7 +1,16 @@
 import urllib.request
 import json
 import os
+import socket
 from dotenv import load_dotenv
+
+# --- IPv6 kikényszerített letiltása (csak IPv4 használata) ---
+old_getaddrinfo = socket.getaddrinfo
+def new_getaddrinfo(*args, **kwargs):
+    responses = old_getaddrinfo(*args, **kwargs)
+    return [response for response in responses if response[0] == socket.AF_INET]
+socket.getaddrinfo = new_getaddrinfo
+# ---------------------------------------------------------------
 
 load_dotenv()
 
